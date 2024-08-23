0d0b0cbdac61729a1fa9793d3da91ed789dc7f0d
Pedro Coelho <pedrocoelho.nascimento@gmail.com>
2024-08-23 19:12:30 +0000

    api fetching produtos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import ListaProdutos from './Componentes/ListaProdutos'
+import axios from 'axios';
 
-const produtos = [{
+/* const produtos = [{
   nome: "Teste",
   preco: 10.00,
 },
 {
   nome: "Teste2",
   preco: 20.00,
-}];
+}]; */
 
 function App() {
+  const [produtos, setProdutos] = useState([]);
+  useEffect(() => { carregaProdutos(); }, []);
+  function carregaProdutos() {
+    axios.get('https://app-api-tapwm.onrender.com/api/produtos').then(res => { setProdutos(res.data) });
+  };
   return (
     <div>
       <h1>Lista Produtos</h1>
-      <ListaProdutos produtos = {produtos} />
+      <ListaProdutos produtos={produtos} />
     </div>
   );
 }
